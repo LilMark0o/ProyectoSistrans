d1b1fc85a8247bd435451a9f9c55740ac47104f2
JhostinAleck <ja.sanchezm12@uniandes.edu.co>
2023-10-01 11:27:54 -0500

    cambios

---
+++ src/main/java/uniandes/edu/co/proyecto/controller/CustomErrorController.java
@@ -0,0 +1,34 @@
+package uniandes.edu.co.proyecto.controller;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.HttpServletRequest;
+
+@Controller
+public class CustomErrorController implements ErrorController {
+
+    @RequestMapping("/error")
+    public String handleError(HttpServletRequest request) {
+        // Obtener el código de error y definir la lógica según lo necesites
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        if (status != null) {
+            int statusCode = Integer.parseInt(status.toString());
+            if(statusCode == HttpStatus.NOT_FOUND.value()) {
+                return "error-404";
+            }
+            else if(statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+                return "error-500";
+            }
+        }
+        return "error";
+    }
+
+   // @Override
+    //public String getErrorPath() {
+      //  return "/error";
+    //}
+}
