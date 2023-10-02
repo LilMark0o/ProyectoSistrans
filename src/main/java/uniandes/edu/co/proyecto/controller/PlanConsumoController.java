ab4257da9cf750d4bfa141d0e9c75bc204ef5cca
Marco Alejandro Ramírez <ma.ramirez23@uniandes.edu.co>
2023-10-01 23:16:18 -0500

    Plan COnsumo

--- src/main/java/uniandes/edu/co/proyecto/controller/PlanConsumoController.java
+++ src/main/java/uniandes/edu/co/proyecto/controller/PlanConsumoController.java
@@ -1,62 +1,59 @@
 package uniandes.edu.co.proyecto.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import uniandes.edu.co.proyecto.modelo.PlanConsumo;
-import uniandes.edu.co.proyecto.repositorio.PlanConsumoRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import uniandes.edu.co.proyecto.modelo.*;
+import uniandes.edu.co.proyecto.repositorio.*;
 
 import java.util.Collection;
 
-@RestController
-@RequestMapping("/api/planesconsumo")
+@Controller
 public class PlanConsumoController {
 
     @Autowired
-    private PlanConsumoRepository planConsumoRepository;
+    private PlanConsumoRepository PlanConsumosRepository;
+    @Autowired
+    private HotelRepository hotelRepository;
 
-    @GetMapping
-    public Collection<PlanConsumo> listarPlanesConsumo() {
-        return planConsumoRepository.darPlanesConsumo();
+    @GetMapping("/planConsumo")
+    public String PlanConsumos(Model model) {
+        model.addAttribute("planConsumo", PlanConsumosRepository.darPlanesConsumo());
+        return "planConsumo";
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<PlanConsumo> obtenerPlanConsumoPorId(@PathVariable Integer id) {
-        PlanConsumo planConsumo = planConsumoRepository.darPlanConsumoPorId(id);
-        if (planConsumo != null) {
-            return new ResponseEntity<>(planConsumo, HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
+    @GetMapping("/planConsumo/new")
+    public String bebidaForm(Model model) {
+        model.addAttribute("planConsumo", new PlanConsumo());
+        return "planConsumoNuevo";
     }
 
-    @PostMapping
-    public ResponseEntity<PlanConsumo> crearPlanConsumo(@RequestBody PlanConsumo planConsumo) {
-        planConsumoRepository.insertarPlanConsumo(planConsumo.getId(), planConsumo.getDescripcion(),
-                planConsumo.getDescuento(), planConsumo.getHotel().getId());
-        return new ResponseEntity<>(planConsumo, HttpStatus.CREATED);
+    @PostMapping("/planConsumo/new/save")
+    public String PlanConsumosGuardar(@ModelAttribute("tipodedocumento") String tipodedocumento,
+            @ModelAttribute("id") Integer id, @ModelAttribute("descripcion") String descripcion,
+            @ModelAttribute("descuento") Integer descuento, @ModelAttribute("hotel_id") Integer hotel_id) {
+        PlanConsumo PlanConsumo = new PlanConsumo();
+        PlanConsumo.setId(id);
+        PlanConsumo.setDescripcion(descripcion);
+        PlanConsumo.setDescuento(descuento);
+        Hotel hotel = hotelRepository.darHotelPorId(hotel_id);
+        PlanConsumo.setHotel(hotel);
+        PlanConsumosRepository.insertarPlanConsumo(PlanConsumo.getId(),
+                PlanConsumo.getDescripcion(),
+                PlanConsumo.getDescuento(),
+                PlanConsumo.getHotel().getId());
+        return "redirect:/planConsumo";
     }
 
-    @PutMapping("/{id}")
-    public ResponseEntity<PlanConsumo> actualizarPlanConsumo(@PathVariable Integer id,
-            @RequestBody PlanConsumo planConsumo) {
-        if (planConsumoRepository.darPlanConsumoPorId(id) != null) {
-            planConsumoRepository.actualizarPlanConsumo(planConsumo.getId(), planConsumo.getDescripcion(),
-                    planConsumo.getDescuento(), planConsumo.getHotel().getId());
-            return new ResponseEntity<>(planConsumo, HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-    }
-
-    @DeleteMapping("/{id}/delete")
-    public ResponseEntity<Void> eliminarPlanConsumo(@PathVariable Integer id) {
-        if (planConsumoRepository.darPlanConsumoPorId(id) != null) {
-            planConsumoRepository.eliminarPlanConsumo(id);
-            return new ResponseEntity<>(HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
+    @GetMapping("/planConsumo/{id}/edit")
+    public String PlanConsumosEditarForm(@PathVariable("id") Integer id, Model model) {
+        // PlanConsumosRepository.eliminarPlanConsumo(id);
+        model.addAttribute("planConsumo", new PlanConsumo());
+        return "planConsumoNuevo";
     }
 }
